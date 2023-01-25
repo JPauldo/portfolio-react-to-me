1eeca7f57cfa777f2ad2615020f195306b28b91f
KingdomSeeker328 <30511892+KingdomSeeker328@users.noreply.github.com>
2023-01-25 18:36:30 -0500

    Added the wireframing for the PortfolioContainer component

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,6 +1,34 @@
-// import React, { useState } from 'react';
+import React, { useState } from 'react';
+import Footer from "./Footer";
+import Header from "./Header";
+import AboutMe from './pages/AboutMe';
+import Contact from './pages/Contact';
+import Portfolio from './pages/Portfolio';
+import Resume from './pages/Resume';
 
 export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState('About Me');
 
-  return;
+  const renderPage = () => {
+    switch (currentPage) {
+      case 'Contact':
+        return <Contact />;
+      case 'Portfolio':
+        return <Portfolio />;
+      case 'Resume':
+        return <Resume />;
+      default:
+        return <AboutMe />;
+    }
+  }
+
+  const handlePageSwitch = (page) => setCurrentPage((page));
+
+  return (<>
+    <Header currentPage={ currentPage } handlePageSwitch={ handlePageSwitch } />
+    <main className="container">
+      {renderPage()}
+    </main>
+    <Footer />
+  </>);
 }
